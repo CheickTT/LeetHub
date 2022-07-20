f46ac38682bef0c2bbc311aa266f3eaa3ea9899f
Albert Terc <albytterc@gmail.com>
2022-07-20 16:41:11 +0000

    added flask files

---
+++ flasksite/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_behind_proxy import FlaskBehindProxy
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_github import GitHub
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SQLALCHEMY_TRACK_MODIFICATIONS = False,
+        SECRET_KEY = '063b5d59f24fbf66d126cfb5e661902f',
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+        # INSERT GITHUB CLIENT ID AND SECRET HERE
+    )
+
+    return app
+
+login_manager = LoginManager()
+
+app = create_app()
+proxied = FlaskBehindProxy(app)
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+github = GitHub(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flasksite import routes
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='leethub',
+    packages=['flasksite'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
